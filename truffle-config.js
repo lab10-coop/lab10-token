db9b02da1b1ba1b0cec5b11a636e70b86d8d69ce
didi <git@d10r.net>
2018-12-10 23:11:20 +0100

    updated package-lock, added sigma1 network

--- truffle-config.js
+++ truffle-config.js
@@ -51,6 +51,12 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
       gasPrice: 1000000000,
+    },
+    sigma1: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8565,            // Standard Ethereum port (default: none)
+      network_id: "246529",       // Any network (default: none)
+      gasPrice: 1000000000,
     }
 
     // Another network with more advanced options...
