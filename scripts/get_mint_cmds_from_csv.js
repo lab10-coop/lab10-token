a2b3ec217daa4afc9aea751fe33bb124e71cbef6
didi <git@d10r.net>
2018-12-11 11:59:47 +0100

    added helper script to get mint commands from csv, refs #4

---
+++ scripts/get_mint_cmds_from_csv.js
@@ -0,0 +1,63 @@
+// parses a csv file "input.csv" (can be a symlink) for minting tasks
+// expected format: amount, "address"
+// if the address field is empty, it's skipped
+// outputs a list of commands to be executed in a truffle5 console
+// with the token contract initialized at variable lab10
+// and with the contract controller unlocked
+
+const fs = require('fs');
+const parse = require('csv-parse');
+const web3 = require('web3');
+
+// have an upper limit for accepted amounts. Choosen somewhat arbitrarily
+const MAX_TOKEN_PER_TASK = 500000;
+
+let goodTasks = [];
+let badTasks = [];
+let executedTasks = [];
+
+function parseTasks() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream('input.csv')
+            .pipe(parse({delimiter: ','}))
+            .on('data', (row) => {
+                const task = {
+                    amount: row[0],
+                    receiver: row[1]
+                };
+                
+                if((! isNaN(task.amount) && task.amount >= 0 && task.amount <= MAX_TOKEN_PER_TASK)
+                    && (web3.utils.isAddress(task.receiver))) {
+                    goodTasks.push(task);
+                } else {
+                    badTasks.push(task);
+                }
+                    
+            })
+            .on('end', () => {
+                console.log(`${goodTasks.length} goodTasks: ${JSON.stringify(goodTasks, null, 2)}`);
+                console.log(`${badTasks.length} badTasks: ${JSON.stringify(badTasks, null, 2)}`);
+                resolve();
+            });
+    });
+}
+
+async function printMintCommands(tasks) {
+    console.log(`
+
+    ==================
+    commands to execute:
+    ==================
+    `);
+    
+    for(const task of tasks) {
+        console.log(
+`await lab10.mint("${task.receiver}", web3.utils.toWei("${task.amount}"))`
+        );
+    };
+}
+
+(async () => {
+    await parseTasks();
+    printMintCommands(goodTasks);
+})();
